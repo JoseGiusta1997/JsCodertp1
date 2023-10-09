62ebb48b3f5c8353896bfb2d2ad5c7a8b2291533
jose giusta <josegiusta97@gmail.com>
2023-10-09 18:54:34 -0300

    tp1 JS Jose Giusta

---
+++ main.js
@@ -0,0 +1,15 @@
+
+    alert("Bienvenidos. Esta pagina te permite calcular la tasa de interés de comprar en cuotas o de contado")
+    alert("La suscripción gratuita solo se puede utilizar 3 veces, luego de eso tenes que registrarte")
+    for(let intento = 1; intento <=3 ; intento++)
+        {
+        let costoContado = prompt("Ingrese el monto de contado");
+        let costoCuotas = prompt("Ingrese el monto en cuotas");
+        let tasaInteres = (costoCuotas/costoContado)*100;
+        if (tasaInteres > 100){
+        alert(`Intento ${intento} La tasa que estas pagando es de: ${tasaInteres}%`);
+        }else{
+        alert ("La tasa es inferior a 1, verifique los datos");
+        }
+        }
+    alert("Intentos terminados, favor de registrarte");
